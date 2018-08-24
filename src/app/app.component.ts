73d8aa4832222805e2fdc547696a798b6bad94de
Radhika <rdkagnihotri@gmail.com>
2018-08-24 13:27:26 +0530

    dnd with nested list and order list from component json object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dndProj';
-  
+  orderableLists = [['Item 1a', 'Item 2a', 'Item 3a'], ['Item 1b', 'Item 2b', 'Item 3b']];
+  orderableLists1 = ['Item 1c', 'Item 2c', 'Item 3c'];
+  nestedLists = [
+    {
+      "label": "Item 1",
+      "children": []
+    },
+    {
+      "label": "Item 2",
+      "children": [
+        {
+          "label": "Item 2a",
+          "children": []
+        },
+        {
+          "label": "Item 2b",
+          "children": []
+        },
+        {
+          "label": "Item 2c",
+          "children": []
+        }
+      ]
+    },
+    {
+      "label": "Item 3",
+      "children": [
+        {
+          "label": "Item 3a",
+          "children": []
+        },
+        {
+          "label": "Item 3b",
+          "children": []
+        },
+        {
+          "label": "Item 3c",
+          "children": ["hello"]
+        }
+      ]
+    }
+  ]
 }
